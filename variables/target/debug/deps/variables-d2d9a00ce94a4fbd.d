19158ed7adc9f46a4edf0f897e7cabfdb47846a0
Baul <roidinev@gmail.com>
2017-07-23 15:37:32 +0800

    varialbes example

---
+++ variables/target/debug/deps/variables-d2d9a00ce94a4fbd.d
@@ -0,0 +1,5 @@
+/Users/baojg/exrust/variables/target/debug/deps/variables-d2d9a00ce94a4fbd: src/main.rs
+
+/Users/baojg/exrust/variables/target/debug/deps/variables-d2d9a00ce94a4fbd.d: src/main.rs
+
+src/main.rs:
