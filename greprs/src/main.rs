185647920ad7f7b85da832cd6fbfa959682a8175
Baul <roidinev@gmail.com>
2017-07-23 17:27:59 +0800

    add test code for greprs

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,23 +1,18 @@
+//use std::env;
+//use std::fs::File;
+//use std::io::prelude::*;
+//use std::process;
+//
+//use std::error::Error;
+//
+extern crate greprs;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
 
-use std::error::Error;
-
-
+use greprs::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
-//    println!("{:?}", args);
-//    let query = &args[1];
-//    let filename = &args[2];
-//
-//    let (query, filename) = parse_config(&args);
-//    println!("Searching for {}", query);
-//    println!("In file {}", filename);
-//    let mut f = File::open(filename).expect("file not found");
-//    let config = parse_config(&args);
-//    let config = Config::new(&args);
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -25,79 +20,107 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-//    let mut f = File::open(config.filename).expect("file not found");
-//
-//    let mut contents = String::new();
-//    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-//    //fn read_to_string(&mut self, buf: &mut String) -> Result<usize>
-//
-//    println!("With text:\n{}", contents);
-//    run(config);
-   if let Err(e) = run(config) {
+
+    if let Err(e) = greprs::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
     }
-
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
 
-//fn parse_config(args: &[String]) -> (&str, &str) {
-//    let query = &args[1];
-//    let filename = &args[2];
+//fn main() {
+//    let args: Vec<String> = env::args().collect();
+////    println!("{:?}", args);
+////    let query = &args[1];
+////    let filename = &args[2];
+////
+////    let (query, filename) = parse_config(&args);
+////    println!("Searching for {}", query);
+////    println!("In file {}", filename);
+////    let mut f = File::open(filename).expect("file not found");
+////    let config = parse_config(&args);
+////    let config = Config::new(&args);
+//    let config = Config::new(&args).unwrap_or_else(|err| {
+//        println!("Problem parsing arguments: {}", err);
+//        process::exit(1);
+//    });
 //
-//    //(query, filename)//tuple
-//}
-
-//fn parse_config(args: &[String]) -> Config {
-//    let query = args[1].clone();
-//    let filename = args[2].clone();
+//    println!("Searching for {}", config.query);
+//    println!("In file {}", config.filename);
+////    let mut f = File::open(config.filename).expect("file not found");
+////
+////    let mut contents = String::new();
+////    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+////    //fn read_to_string(&mut self, buf: &mut String) -> Result<usize>
+////
+////    println!("With text:\n{}", contents);
+////    run(config);
+//   if let Err(e) = run(config) {
+//        println!("Application error: {}", e);
 //
-//    Config { query, filename }
+//        process::exit(1);
+//    }
+//
 //}
+//
+//struct Config {
+//    query: String,
+//    filename: String,
+//}
+//
+////fn parse_config(args: &[String]) -> (&str, &str) {
+////    let query = &args[1];
+////    let filename = &args[2];
+////
+////    //(query, filename)//tuple
+////}
+//
+////fn parse_config(args: &[String]) -> Config {
+////    let query = args[1].clone();
+////    let filename = args[2].clone();
+////
+////    Config { query, filename }
+////}
+////impl Config {
+////    fn new(args: &[String]) -> Config {
+////        if args.len() < 3 {
+////            panic!("not enough arguments");
+////        }
+////        let query = args[1].clone();
+////        let filename = args[2].clone();
+////
+////        Config { query, filename }
+////    }
+////}
 //impl Config {
-//    fn new(args: &[String]) -> Config {
+//    fn new(args: &[String]) -> Result<Config, &'static str> {
 //        if args.len() < 3 {
-//            panic!("not enough arguments");
+//            return Err("not enough arguments");
 //        }
+//
 //        let query = args[1].clone();
 //        let filename = args[2].clone();
 //
-//        Config { query, filename }
+//        Ok(Config { query, filename })
 //    }
 //}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-//fn run(config: Config) {
-//    let mut f = File::open(config.filename).expect("file not found");
+////fn run(config: Config) {
+////    let mut f = File::open(config.filename).expect("file not found");
+////
+////    let mut contents = String::new();
+////    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+////
+////    println!("With text:\n{}", contents);
+////}
+//
+//fn run(config: Config) -> Result<(), Box<Error>> {
+//    let mut f = File::open(config.filename)?;//?
 //
 //    let mut contents = String::new();
-//    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+//    f.read_to_string(&mut contents)?;
 //
 //    println!("With text:\n{}", contents);
+//
+//    Ok(())
 //}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;//?
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
